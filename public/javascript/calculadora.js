3cb440ad0b159c1f300e07433a938e7b6a35189f
Kevin Castro <84404365+kevcorti@users.noreply.github.com>
2023-06-15 13:58:11 -0500

    Update calculadora.js

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -4,7 +4,12 @@ const funcionPredeterminada = () => {
     evento.preventDefault();
     let vph = parseFloat(document.getElementById('valorPorHora').value);
     let iph = parseFloat(document.getElementById('interfazHoras').value);
-    let resultado = vph + iph;
+    let dhtml = parseFloat(document.getElementById('interfazHoras').value);
+    let estcss = parseFloat(document.getElementById('interfazHoras').value);
+    let fad = parseFloat(document.getElementById('interfazHoras').value);
+    let corr = parseFloat(document.getElementById('interfazHoras').value);
+  
+    let resultado = vph + iph + dhtml + estcss + fad +corr;
     document.getElementById('valorTotal').value = resultado;
   });
 };
