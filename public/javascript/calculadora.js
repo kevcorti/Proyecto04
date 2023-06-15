91ac54a6e2c82cc63d4098b7ec5b195029d4c910
Kevin Castro <84404365+kevcorti@users.noreply.github.com>
2023-06-15 14:00:56 -0500

    Update calculadora.js

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -4,10 +4,10 @@ const funcionPredeterminada = () => {
     evento.preventDefault();
     let vph = parseFloat(document.getElementById('valorPorHora').value);
     let iph = parseFloat(document.getElementById('interfazHoras').value);
-    let dhtml = parseFloat(document.getElementById('interfazHoras').value);
-    let estcss = parseFloat(document.getElementById('interfazHoras').value);
-    let fad = parseFloat(document.getElementById('interfazHoras').value);
-    let corr = parseFloat(document.getElementById('interfazHoras').value);
+    let dhtml = parseFloat(document.getElementById('DesarrolloHTML').value);
+    let estcss = parseFloat(document.getElementById('estiloCSS').value);
+    let fad = parseFloat(document.getElementById('adicionales').value);
+    let corr = parseFloat(document.getElementById('correcciones').value);
   
     let resultado = vph + iph + dhtml + estcss + fad +corr;
     document.getElementById('valorTotal').value = resultado;
