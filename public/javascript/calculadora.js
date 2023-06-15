0beb16c5abca177571c80c726df3e6f01ad5ec17
Kevin Castro <84404365+kevcorti@users.noreply.github.com>
2023-06-15 15:21:45 -0500

    Update calculadora.js

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -5,7 +5,7 @@ const funcionPredeterminada = () => {
     let vph = parseFloat(document.getElementById('valorPorHora').value);
     let iph = parseFloat(document.getElementById('interfazHoras').value);
     let dhtml = parseFloat(document.getElementById('DesarrolloHTML').value);
-    let estCSS = parseFloat(document.getElementById('DesarrolloHTML').value);
+    let estCSS = parseFloat(document.getElementById('estiloCSS').value);
     let ad = parseFloat(document.getElementById('adicionales').value);
     let corr = parseFloat(document.getElementById('correcciones').value);
     let resultado = vph + iph + dhtml + estCSS + ad + corr;
